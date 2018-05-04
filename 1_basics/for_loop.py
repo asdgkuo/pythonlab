2ff5963987be10c75079d29f4a02cb68c031d5ee
asdgkuo <35054416+asdgkuo@users.noreply.github.com>
2018-05-04 16:35:50 +0800

    pythonlab

---
+++ 1_basics/for_loop.py
@@ -0,0 +1,27 @@
+def pr10():
+    for i in range(0,11):
+        print(i,end=" ")
+    print()
+
+def prNums(n):
+    for i in range(0,n+1):
+        print(i,end=" ")
+    print()
+
+def test():
+    prNums(15)
+    pr10()
+
+def pr5():
+    i=0
+    while i<=10:
+        print(i,end=" ")
+        i += 1
+    print()
+
+def test():
+    pr5()
+    pr10()
+    prNums
+
+test()
---
+++ 1_basics/ticket.py
@@ -0,0 +1,24 @@
+def ticket1(age):
+    if age<15:
+        print("chidren")
+    elif age<65:
+        print("adults")
+    else:
+        print("olds")
+
+def ticket2(age):
+    if age<15: print("kids")
+    elif age<65: print("adults")
+    else: print("oldman")
+
+def ticket3(age):
+    tickettype:child
+def test():
+    ticket1(20)
+    ticket1(10)
+    ticket1(30)
+    ticket2(10)
+    ticket2(30)
+    ticket2(67)
+
+test()
